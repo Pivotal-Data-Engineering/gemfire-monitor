f223f6671275a748854a2a5e5e14789a21ff84af
Randy May <randy@mathysphere.com>
2015-11-25 11:04:30 -0500

    added printout of excessive threads and updated README

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
 #Overview#
 
-GemFire monitor  connects to a cluster via JMX
-and logs certain metrics periodically.  It is built
-with as few dependencies as possible so that it can
+GemFire monitor  connects to a cluster via JMX and logs certain metrics 
+periodically.  It is built with as few dependencies as possible so that it can
 be used by GemFire users who do not use Spring, etc..
+
+GemFire monitor does not 
+
+#Build Instructions#
+`mvn package`
+
+This will produce an archive at `target/gemfire-monitor-VERSION-runtime.tar.gz`
+which contains everything needed to run the monitor.
+
+#Installation and Configuration#
+Unpack the archive on a server that has access to the JMX manager 
+(usually this is also a locator).
+
+Output is controlled by the `log4j.properties` file (see instructions in file).
+The file includes configurations for sending the output to the console or 
+to a file, or both.  More advanced configurations including log rolling can
+be configured using log4j 1.2 (see https://logging.apache.org/log4j/1.2/manual.html 
+for information)
+
+#Running#
+`./gemfire-monitor.sh jmx-host jmx-port [jmx-user jmx-pass]`
+
+The monitor will publish statistics about the cluster every 20s 
+
+
+__The following conditions will cause a special warning to be logged, which can 
+be identified by the presence of the string  "WARN"__ 
+* thread count > 2000
+
+Use CTRL-C to stop the monitor if it is running in the foreground or 
+`kill` if it is in the background (It's better not to use `kill -9` because
+this will not allow gemfire-monitor to close connections before exiting)
 
 #To Do#
 * add support for a backup JMX manager
-* not sure if having a non-daemon thread will be a good thing 
-if this runs in the app server
-* packaging
+* if this is run inside an app server, revisit the idea of using a non-daemon
+thread
+* make the interval configurable
 
 
 
--- src/main/java/io/pivotal/pde/gemfire/monitor/GemFireMonitor.java
+++ src/main/java/io/pivotal/pde/gemfire/monitor/GemFireMonitor.java
@@ -23,9 +23,9 @@ import com.gemstone.gemfire.management.MemberMXBean;
 
 public class GemFireMonitor extends Thread {
 
-	private static Logger log =LoggerFactory.getLogger(GemFireMonitor.class);
-	
-	private Logger metricLog =null;
+	private static Logger log = LoggerFactory.getLogger(GemFireMonitor.class);
+
+	private Logger metricLog = null;
 	private JMXServiceURL jmxURL;
 	private AtomicBoolean running = null;
 	private HashMap<String, Serializable> jmxEnv = null;
@@ -58,66 +58,74 @@ public class GemFireMonitor extends Thread {
 			try {
 				jmxc = JMXConnectorFactory.connect(jmxURL, jmxEnv);
 				MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
-				ObjectName dsOname = new ObjectName(
-						"GemFire:service=System,type=Distributed");
-				DistributedSystemMXBean distributedSystemBean = JMX.newMXBeanProxy(
-						mbsc, dsOname, DistributedSystemMXBean.class);
-				
+				ObjectName dsOname = new ObjectName("GemFire:service=System,type=Distributed");
+				DistributedSystemMXBean distributedSystemBean = JMX.newMXBeanProxy(mbsc, dsOname,
+						DistributedSystemMXBean.class);
+
 				logMetric(dsOname.toString(), "MemberCount", distributedSystemBean.getMemberCount());
 				logMetric(dsOname.toString(), "JVMPauses", distributedSystemBean.getJVMPauses());
 				logMetric(dsOname.toString(), "NumClients", distributedSystemBean.getNumClients());
-				logMetric(dsOname.toString(), "TotalRegionEntryCount", distributedSystemBean.getTotalRegionEntryCount());
-				
-				ObjectName []memberONames = distributedSystemBean.listMemberObjectNames();
-				for(ObjectName memberOName: memberONames){
+				logMetric(dsOname.toString(), "TotalRegionEntryCount",
+						distributedSystemBean.getTotalRegionEntryCount());
+
+				ObjectName[] memberONames = distributedSystemBean.listMemberObjectNames();
+				for (ObjectName memberOName : memberONames) {
 					MemberMXBean memberMBean = JMX.newMXBeanProxy(mbsc, memberOName, MemberMXBean.class);
-					
-					logMetric(memberOName.toString(), "NumThreads", memberMBean.getNumThreads());
-					logMetric(memberOName.toString(), "HostCpuUsage", memberMBean.getHostCpuUsage( ) );
+
+					int numThreads = memberMBean.getNumThreads();
+					logMetric(memberOName.toString(), "NumThreads", numThreads);
+					logMetric(memberOName.toString(), "HostCpuUsage", memberMBean.getHostCpuUsage());
 					logMetric(memberOName.toString(), "FreeHeap", String.format("%dM", memberMBean.getFreeMemory()));
+
+					if (numThreads > 2000) {
+						log.warn("member " + memberMBean.getName() + " (pid " + memberMBean.getProcessId() + " ) on " + memberMBean.getHost() +  " has HIGH THREAD COUNT: " + numThreads + " thread list follows");
+						for (String tname : memberMBean.fetchJvmThreads())
+							logMetric(memberOName.toString(), "ThreadName", tname);
+					}
 				}
-				
-				ObjectName []cacheServerONames = distributedSystemBean.listCacheServerObjectNames();
-				for(ObjectName cacheServerOName: cacheServerONames){
+
+				ObjectName[] cacheServerONames = distributedSystemBean.listCacheServerObjectNames();
+				for (ObjectName cacheServerOName : cacheServerONames) {
 					CacheServerMXBean csBean = JMX.newMBeanProxy(mbsc, cacheServerOName, CacheServerMXBean.class);
-					logMetric(cacheServerOName.toString(),"ClientCount", csBean.getCurrentClients());
-					logMetric(cacheServerOName.toString(),"ClientConnectionCount", csBean.getClientConnectionCount());
-					logMetric(cacheServerOName.toString(),"MaxClientConnectionCount", csBean.getMaxConnections());
-					logMetric(cacheServerOName.toString(),"ConnectionThreads", csBean.getConnectionThreads());
-					logMetric(cacheServerOName.toString(),"MaxConnectionThreads", csBean.getMaxThreads());				
+					logMetric(cacheServerOName.toString(), "ClientCount", csBean.getCurrentClients());
+					logMetric(cacheServerOName.toString(), "ClientConnectionCount", csBean.getClientConnectionCount());
+					logMetric(cacheServerOName.toString(), "MaxClientConnectionCount", csBean.getMaxConnections());
+					logMetric(cacheServerOName.toString(), "ConnectionThreads", csBean.getConnectionThreads());
+					logMetric(cacheServerOName.toString(), "MaxConnectionThreads", csBean.getMaxThreads());
 				}
 			} catch (IOException iox) {
 				log.error("GemFire monitor failure", iox);
 			} catch (MalformedObjectNameException mfx) {
 				log.error("Internal error in GemFire monitor - will shut down", mfx);
-				running.set(false);  // will keep the shutdown method from waiting
-				break; //BREAK
+				running.set(false); // will keep the shutdown method from
+									// waiting
+				break; // BREAK
 			} finally {
-				try { 
+				try {
 					jmxc.close();
-				} catch(IOException iox){
+				} catch (IOException iox) {
 					log.error("error closing JMX connector", iox);
 				}
 			}
-			
+
 			try {
 				Thread.sleep(20000);
-			} catch(InterruptedException ix){
+			} catch (InterruptedException ix) {
 				// OK
 			}
 		}
 	}
-	
-	private void logMetric(String subject, String metric, Object value){
+
+	private void logMetric(String subject, String metric, Object value) {
 		metricLog.info(String.format("%s|%s|%s", subject, metric, value));
 	}
 
 	public void shutdown() {
-		if (running.compareAndSet(true, false)){
+		if (running.compareAndSet(true, false)) {
 			this.interrupt();
 			try {
 				this.join(40000);
-			} catch(InterruptedException ix){
+			} catch (InterruptedException ix) {
 				log.warn("could not verify shutdown");
 				return;
 			}
@@ -127,11 +135,11 @@ public class GemFireMonitor extends Thread {
 
 	// jmxHost, jmxPort, userName, password
 	public static void main(String[] args) {
-		if (args.length < 2){
+		if (args.length < 2) {
 			printUsage();
 			System.exit(1);
 		}
-		
+
 		String jmxHost = null;
 		int jmxPort = 0;
 		String jmxUser = null;
@@ -140,47 +148,46 @@ public class GemFireMonitor extends Thread {
 		jmxHost = args[0];
 		try {
 			jmxPort = Integer.parseInt(args[1]);
-		} catch(NumberFormatException x){
+		} catch (NumberFormatException x) {
 			log.error("second argument should be a port number - provided value: " + args[1]);
-			System.exit(1);  //EXIT
+			System.exit(1); // EXIT
 		}
 
-		
-		if (args.length == 3){
+		if (args.length == 3) {
 			log.error("jmx username argument provided but jmx password argument not provided");
-			System.exit(1); //EXIT
+			System.exit(1); // EXIT
 		}
 
-		if (args.length > 2){
+		if (args.length > 2) {
 			jmxUser = args[2];
 			jmxPass = args[3];
 		}
-		
-		if (args.length > 4){
+
+		if (args.length > 4) {
 			log.error("unrecognized extra arguments provided");
 			System.exit(1);
 		}
-		
+
 		try {
 			GemFireMonitor m = new GemFireMonitor(jmxHost, jmxPort, jmxUser, jmxPass);
-			Runtime.getRuntime().addShutdownHook(new Thread(){
+			Runtime.getRuntime().addShutdownHook(new Thread() {
 
 				@Override
 				public void run() {
 					m.shutdown();
 				}
-				
+
 			});
 			m.start();
-			
+
 			System.out.println("monitor started, press Ctrl-C to stop");
-		} catch(Exception x){
+		} catch (Exception x) {
 			log.error("unexpected exception in monitor, will exit", x);
-			System.exit(1); //EXIT
+			System.exit(1); // EXIT
 		}
 	}
 
-	private  static void printUsage(){
+	private static void printUsage() {
 		log.info("usage: gemfire-monitor jmx-host jmx-port [jmx-user jmx-password]");
 	}
 }
